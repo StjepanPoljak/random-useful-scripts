3cf0c25caed5a0f8615540d996c9424dd90bee99
Stjepan Poljak <36042076+StjepanPoljak@users.noreply.github.com>
2019-03-04 10:31:17 +0100

    Find syscall by name.

--- find_syscall.sh
+++ find_syscall.sh
@@ -19,25 +19,43 @@ then
 	elif ! [ -z "$1" ]
 	then
 		UNISTD64="$(./find_unistd.sh -d)/unistd_64.h"
-		NRS="$(cat "$UNISTD64" | grep " $1"$ | grep "__NR_" )"
 
-		NRS_COUNT="$( echo "$NRS" | wc -l )"
+		case $1 in
+			"SYS_"*)
+				NOSYS="${1##*SYS}"
+				NRS="$(cat "$UNISTD64" | grep "$NOSYS")"
 
-		if [ "$NRS_COUNT" = "1" ]
-		then
-			CUT_LEAD="${NRS##*#define }"
-			NR="${CUT_LEAD% *}"
+				NRS_COUNT="$( echo "$NRS" | wc -l )"
 
-			RES_LINES="$(cat "$RES_INC" | grep "$NR"$ )"
+				if [ "$NRS_COUNT" = "1" ]
+				then
+					echo "${NRS##*#define __NR$NOSYS }"
+				else
+					exit 1
+				fi
+			;;
+			*)
+				NRS="$(cat "$UNISTD64" | grep " $1"$ | grep "__NR_" )"
 
-			echo "$RES_LINES\n" |
-			while read -r RES_LINE; do
-				NO_DEFINE="${RES_LINE##*#define }"
-				echo "${NO_DEFINE%% *}"
-				break
-			done
-		else
-			exit 1
-		fi
+				NRS_COUNT="$( echo "$NRS" | wc -l )"
+
+				if [ "$NRS_COUNT" = "1" ]
+				then
+					CUT_LEAD="${NRS##*#define }"
+					NR="${CUT_LEAD% *}"
+
+					RES_LINES="$(cat "$RES_INC" | grep "$NR"$ )"
+
+					echo "$RES_LINES\n" |
+					while read -r RES_LINE; do
+						NO_DEFINE="${RES_LINE##*#define }"
+						echo "${NO_DEFINE%% *}"
+						break
+					done
+				else
+					exit 2
+				fi
+			;;
+		esac
 	fi
 fi
