7276f082b756f51effb19066774959381b2b343c
Stjepan Poljak <36042076+StjepanPoljak@users.noreply.github.com>
2019-03-18 10:11:39 +0100

    Fixes and TODO's for install_vim.sh

--- vim/install_vim.sh
+++ vim/install_vim.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# TODO: we need tmux, make, git, autoconf, ncurses, pkgconfig, keychain, pathogen...
+
 VIM_INSTALL_DIR="$HOME/vim-install"
 
 if ! [ -d "$VIM_INSTALL_DIR" ]
@@ -33,6 +35,21 @@ then
 fi
 
 BUNDLE_DIR="$HOME/.vim/bundle"
+AUTOLOAD_DIR="$HOME/.vim/autoload"
+
+if [ -d "$BUNDLE_DIR" ]
+then
+	rm -rf "$BUNDLE_DIR"
+fi
+
+if [ -d "$AUTOLOAD_DIR" ]
+then
+	rm -rf "$AUTOLOAD_DIR"
+fi
+
+mkdir -p "$AUTOLOAD_DIR" "$BUNDLE_DIR" && curl -LSso $HOME/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+echo -ne "execute pathogen#infect()\nsyntax on\nfiletype plugin indent on" > "$HOME/.vimrc"
 
 git clone https://github.com/scrooloose/nerdtree.git "$BUNDLE_DIR/nerdtree"
 
@@ -40,4 +57,4 @@ git clone https://github.com/majutsushi/tagbar "$BUNDLE_DIR/tagbar"
 
 git clone https://github.com/altercation/vim-colors-solarized "$BUNDLE_DIR/vim-colors-solarized"
 
-echo "syntax enable\nset background=dark\ncolorscheme solarized\n\nset number" > "$HOME/.vimrc"
+echo -ne "\n\nsyntax enable\nset background=dark\ncolorscheme solarized\n\nset number" >> "$HOME/.vimrc"
