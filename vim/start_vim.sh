3dd52b90e7dc8cd9652388e4a0c04f883d4295bd
Stjepan Poljak <36042076+StjepanPoljak@users.noreply.github.com>
2019-03-18 10:13:40 +0100

    Fixes for start_vim.sh (non-consistent pane behaviour).

--- vim/start_vim.sh
+++ vim/start_vim.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-SESSION_TAG=vim-stjepan
-SESSION_NAME="Stjepan's vim"
+SESSION_TAG=vim-epljstj
+SESSION_NAME="EPLJSTJ's vim"
 SIDE_WINDOWS_SIZE=40
 
 if ! [ "$1" = "--continue" ]
@@ -13,22 +13,19 @@ tmux has-session -t $SESSION_TAG
 
 if [ $? != 0 ]
 then
-	tmux new-session -s $SESSION_TAG -n "$SESSION_NAME" -d
+	tmux new-session -s $SESSION_TAG -n "$SESSION_NAME" -d -x "$(tput cols)" -y "$(tput lines)"
 
 	tmux split-window -v -t $SESSION_TAG:0
 
-	tmux resize-pane -t $SESSION_TAG:0.1 -y 17
-
 	tmux set mouse on
 
-	tmux send-keys -t $SESSION_TAG:0.0 "vim -c \"set mouse=a\" -c NERDTree -c \"wincmd h\" -c \"vertical resize $SIDE_WINDOWS_SIZE\" -c TagbarToggle -c \"wincmd l\" -c \"wincmd l\" -c \"vertical resize $SIDE_WINDOWS_SIZE\" -c \"wincmd h\"" C-m
-  
-	if [ -f "/usr/bin/keychain" ] && [ -f "$HOME/.ssh/id_rsa" ]
-  then
-    tmux send-keys -t $SESSION_TAG:0.1 'keychain --eval --agents ssh id_rsa' C-m
-  fi
+	tmux send-keys -t $SESSION_TAG:0.1 'keychain --eval --agents ssh id_rsa' C-m
+
+	tmux send-keys -t $SESSION_TAG:0.0 "sleep 1; vim -c \"set mouse=a\" -c NERDTree -c \"wincmd h\" -c \"vertical resize $SIDE_WINDOWS_SIZE\" -c \"wincmd l\" -c TagbarToggle -c \"wincmd l\" -c \"vertical resize $SIDE_WINDOWS_SIZE\" -c \"wincmd h\"" C-m
 
 	tmux select-pane -t $SESSION_TAG:0.0
+
+	tmux resize-pane -t $SESSION_TAG:0.1 -D 15
 fi
 
 tmux attach -t $SESSION_TAG
